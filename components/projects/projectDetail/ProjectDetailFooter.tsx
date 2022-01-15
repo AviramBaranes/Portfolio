4ea646112ccfce02b18306b199ba1c8e04dc2c7f
moneyman01 <tchrocr@gmail.com>
2022-01-15 14:16:28 +0200

    projectDetailFooter component

--- components/projects/projectDetail/ProjectDetailFooter.tsx
+++ components/projects/projectDetail/ProjectDetailFooter.tsx
@@ -1,4 +1,6 @@
 import { FC } from 'react';
+import { TECHNOLOGIES } from '../../../utils/frameworksList';
+import Technology from '../../UI/Technology/Technology';
 
 interface ProjectDetailFooter {
   description: string;
@@ -9,7 +11,25 @@ const ProjectDetailFooter: FC<ProjectDetailFooter> = ({
   description,
   technologies,
 }) => {
-  return <div></div>;
+  const technologiesToDisplay = TECHNOLOGIES.filter((tech) =>
+    technologies.includes(tech.name)
+  );
+  return (
+    <div>
+      <p>{description}</p>
+      {technologiesToDisplay.map((technology, i) => (
+        <div key={technology.alt}>
+          <Technology
+            index={i}
+            alt={technology.alt}
+            src={technology.src}
+            white={technology.white}
+            link={technology.link}
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ProjectDetailFooter;
