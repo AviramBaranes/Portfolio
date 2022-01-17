cf059525598faf83bff034f82c96b61559eca4c7
moneyman01 <tchrocr@gmail.com>
2022-01-17 12:57:55 +0200

    finished ImageGallery

--- components/projects/projectDetail/ImageGallery.tsx
+++ components/projects/projectDetail/ImageGallery.tsx
@@ -2,34 +2,66 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { FC, useState } from 'react';
 
+import classes from '../../../styles/projects/projects.module.scss';
+
 interface ImageGallery {
   photosPaths: string[];
 }
 
 const ImageGallery: FC<ImageGallery> = ({ photosPaths }) => {
-  const [currentImage, setCurrentImage] = useState(
-    photosPaths.find((path) => path.endsWith('.gif')) ||
-      photosPaths[Math.floor(photosPaths.length / 2)]
-  );
+  const [startIndex, setStartIndex] = useState(0);
+
+  function buttonClickHandler(direction: 'forward' | 'backward') {
+    if (direction === 'backward')
+      setStartIndex((prev) => (prev === 0 ? photosPaths.length - 1 : --prev));
+    else
+      setStartIndex((prev) => (prev === photosPaths.length - 1 ? 0 : ++prev));
+  }
+
+  function getPartialArr() {
+    let result: string[] = [];
+    if (photosPaths.length <= 3) return photosPaths;
+    if (startIndex + 3 > photosPaths.length - 1) {
+      result = photosPaths.slice(startIndex);
+      let currentIndex = 0;
+      while (result.length < 3) {
+        result.push(photosPaths[currentIndex]);
+        currentIndex++;
+      }
+      return result;
+    }
+    return [...photosPaths].splice(startIndex, 3);
+  }
 
   return (
     <div>
-      <Link href={process.env.BASE_URL + currentImage}>
-        <Image src={currentImage} width={830} height={400} />
+      <Link href={process.env.BASE_URL + photosPaths[startIndex]}>
+        <Image src={photosPaths[startIndex]} width={830} height={400} />
       </Link>
       <br />
-      {photosPaths.map((src) => {
-        if (src !== currentImage) {
-          return (
+      {photosPaths.length > 3 && (
+        <button onClick={() => buttonClickHandler('backward')}>{'<'}</button>
+      )}
+      {getPartialArr().map((src) => {
+        return (
+          <div
+            key={src}
+            className={
+              src === photosPaths[startIndex] ? classes.CurrentImage : ''
+            }
+          >
             <Image
-              onClick={() => setCurrentImage(src)}
+              onClick={() => setStartIndex(photosPaths.indexOf(src))}
               src={src}
               width={276.6}
               height={133.3}
             />
-          );
-        }
+          </div>
+        );
       })}
+      {photosPaths.length > 3 && (
+        <button onClick={() => buttonClickHandler('forward')}>{'>'}</button>
+      )}
     </div>
   );
 };
